f45664777f5fcd34cb500d6a5dfd937b81b28b82
Sunaljabili <you@example.com>
2021-11-07 19:07:49 +0400

    Completed

---
+++ JS/homework.js
@@ -0,0 +1,31 @@
+// Task:1 Bir function yaziriq,2 ededin ceminin 100 olub
+// olmadigini ve 2 ededden birinin 100 olub olmadigini yoxlayib,
+//geriye true cavabi qaytarsin.
+
+// function equalTo(a,b){
+
+//     if(a+b==100 || a==100 || b==100){
+//         return true;
+//     }
+//   return false;
+// }
+
+// console.log(equalTo(100,10));
+
+// console.log(equalTo(30,50));
+
+// Task:2 Kreditə götürdüyümüz pul və aylıq(necə ay ödəməliyəm)verilir.
+//  Pul bərabər hissələrə bölünərək hər ay verilməlidir.
+// Məs:1000 manat götürülür, 3 ayliq.
+// Ekrana yazdirilmalidir: 1-ci ay 333manat, 2-ci ay 333manat, 3-cu ay 334 manat;
+
+// function getCredit(money,month){
+//     let part = money%month;
+//     let payment = (money-part)/month;
+//     for( var i=1;i<month;i++){
+//         console.log(`${i}-ci ay ${payment}azn`);
+//     }
+//     console.log(`${i}-ci ay ${payment+part} azn`);
+// }
+
+// console.log(getCredit(1000,3));
---
+++ JS/script.js
@@ -0,0 +1,44 @@
+// let hi = "Salam";
+// let name = "Emil";
+// console.log(`olsun ${name}`);
+
+//Array Methods Start  
+// let student = ["Emil", "Veli", "Nazile", "Sunal"];
+
+// console.log(student[0]);
+// console.log(student);
+// console.log(student.toString()); //Arrayi stringe cevirir
+// console.log(student.join(" "))  //Arrayi stringe cevirir,menim qoydugum simvola gore
+// student.pop();
+// student.pop();
+
+// student.shift();
+// student.shift();
+
+// student.unshift("Gunay","Feqan");
+// student[1]="Ehmed";
+// student.push("Eloset","Aygun","Gunay");
+// console.log(student);
+// student.splice(1,2,"Nermin");
+// console.log(student);
+// student.splice(0,0,"Sunal");
+
+// let newstu = student.slice(1);
+// console.log(newstu);
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple","WaterMelon" ,"Mango"];
+// const citrus = fruits.slice(2,5);  
+// console.log(citrus);
+
+// Array Methods End
+
+
+// let letter = "Qarabag Azerbaycandir!";
+
+// console.log(letter.slice(7,12));
+
+let text = "a,b,c,d,e,f";
+// console.log(text);
+const myArray = text.split("");
+
+console.log(myArray);
